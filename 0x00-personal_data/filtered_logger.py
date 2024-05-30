1d8b13a27c85faca0940ebee8a4da147e11efdec
aihassan1 <abdosruor57@gmail.com>
2024-05-31 02:42:44 +0300

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Returns a log message obfuscated"""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}", f"{f}={redaction}{separator}", message)
+        message = re.sub(f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
     return message
 
 
