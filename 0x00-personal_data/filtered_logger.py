00c8a847bf5aaba715551094a0c14a6089d2c1f5
aihassan1 <abdosruor57@gmail.com>
2024-05-31 03:51:28 +0300

    2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 """Write a function called filter_datum that returns the log message obfuscated:"""
-import re
+import re, logging
 from typing import List
 
 
-
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Returns a log message obfuscated"""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
+        message = re.sub(
+            f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message
+        )
     return message
 
 
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    # Update the class to accept a list of strings fields constructor argument.
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format func"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
