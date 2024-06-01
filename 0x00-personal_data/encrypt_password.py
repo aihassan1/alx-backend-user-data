89c46435fd49bb2e99b95a552c3eb0763f312362
aihassan1 <abdosruor57@gmail.com>
2024-06-01 05:47:29 +0300

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """expects one string argument name password
     returns a salted, hashed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
