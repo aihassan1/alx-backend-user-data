7c9204076cef33a5f5d31145b0b57ee0df845c9d
aihassan1 <abdosruor57@gmail.com>
2024-06-05 05:31:24 +0300

    3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""class auth"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header"""
+        return None
+    def current_user(self, request=None) -> TypeVar("User"):
+        """current_user"""
+        return None
