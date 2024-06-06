7ff80b179f48cee0e5ca5691605cc915e168b5c3
aihassan1 <abdosruor57@gmail.com>
2024-06-06 13:57:24 +0300

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,7 +70,10 @@ def check_auth():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) and auth.session_cookie(request):
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
         return None, abort(401)
 
     request.current_user = auth.current_user(request)
