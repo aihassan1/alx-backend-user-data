6e38c8ce5cb8d6ad753f102610c00e1e27e0f6d3
aihassan1 <abdosruor57@gmail.com>
2024-06-06 13:48:08 +0300

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,8 +50,12 @@ def check_auth():
     """checks if auth is needed based on env var"""
     if auth is None:
         return
-    excluded_paths = ["/api/v1/status/",
-                      "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
+    ]
     if not (
         auth.require_auth(
             request.path,
@@ -65,6 +69,9 @@ def check_auth():
 
     if auth.current_user(request) is None:
         abort(403)
+
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
 
     request.current_user = auth.current_user(request)
 
