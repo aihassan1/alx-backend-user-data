5317a8042d3cb032da31fa21c170c8586f498d96
aihassan1 <abdosruor57@gmail.com>
2024-06-06 12:05:30 +0300

    1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,10 @@ if auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "auth":
     auth = Auth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
