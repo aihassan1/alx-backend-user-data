1a7c34b9a8285179a971bd23c94f140b992fe63f
aihassan1 <abdosruor57@gmail.com>
2024-06-13 17:30:26 +0300

    14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,5 +43,16 @@ def login() -> json:
     return response
 
 
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """logs out a user and destroy its session"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(session_id)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
