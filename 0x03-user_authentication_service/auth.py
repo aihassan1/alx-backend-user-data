5aa25d382a1908af7046ea475409996fc3db9659
aihassan1 <abdosruor57@gmail.com>
2024-06-13 18:22:21 +0300

    16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,8 +88,8 @@ class Auth:
         """returns a reset token if the user exists"""
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = uuid4()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
         except Exception:
             raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
