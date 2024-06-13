106226344c5e0e8ab225f0ef49654f52b465bde3
aihassan1 <abdosruor57@gmail.com>
2024-06-13 19:20:09 +0300

    19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,6 +87,7 @@ def get_reset_password_token() -> str:
     except ValueError:
         abort(403)
 
+
 @app.route(rule="/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """update the password"""
